15f4bcbf8698bf1b077883c3aa3c1507fafafeb1
Artem <artchzhen@gmail.com>
2023-05-12 13:52:22 +0900

    initial commit development

---
+++ Project 22/Project 22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 22
+//
+//  Created by Артем Чжен on 12/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
